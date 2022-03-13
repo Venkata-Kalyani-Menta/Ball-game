a48924708071195feb1198f1d774ea6396cbe841
Venkata-Kalyani-Menta <101486327+Venkata-Kalyani-Menta@users.noreply.github.com>
2022-03-13 07:56:59 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,20 +7,20 @@ public class Main
         Scanner sc=new Scanner(System.in);
         int i,j,n,count;
         char a[][];
-        System.out.print("Enter the size of NxN matrix : ");
+        System.out.print("Enter the size of NxN matrix : "); //Create a matrix of size NxN
         n=sc.nextInt();
         a=new char[n][n];
         for(i=0;i<n;i++)
         {
             for(j=0;j<n;j++)
             {
-                if(i==0||i==(n-1)||j==0||j==(n-1))
+                if(i==0||i==(n-1)||j==0||j==(n-1)) //To create wall bricks
                 a[i][j]='W';
                 else 
-                a[i][j]=' ';
-                if((i==6 && j==1)||(i==6 && j==2)||(i==6 && j==4)||(i==6 && j==5))
+                a[i][j]=' '; //To add spaces
+                if((i==6 && j==1)||(i==6 && j==2)||(i==6 && j==4)||(i==6 && j==5)) //To create a ground for ball to bounce
                 a[i][j]='G';
-                if(i==6&&j==3)
+                if(i==6&&j==3) //To create a ball
                 a[i][j]='O';
                 
             }
@@ -28,7 +28,7 @@ public class Main
         }
         while(true)
                 {
-                    System.out.print("Enter the brick's position and the brick type : ");
+                    System.out.print("Enter the brick's position and the brick type : "); //To add additional bricks at required postion
                     int p,q;
                     char x,flag;
                     p=sc.nextInt();
@@ -51,7 +51,7 @@ public class Main
             System.out.println();
         }
         
-      System.out.print("Enter ball count ");
+      System.out.print("Enter ball count "); //How many chances required for bouncing the ball
       count=sc.nextInt();
       System.out.print("Ball count is "+count);
     }
